5f2d68104a8d2c97edd0248836d18ea641a237db
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-12-09 22:45:14 +0800

    Add Zooniverse Grommet Theme

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Box, Grommet, Heading, Paragraph as P } from 'grommet'
+import { base as baseTheme, Box, Grommet, Heading, Paragraph as P } from 'grommet'
+import { deepMerge } from 'grommet/utils'
+import zooTheme from '@zooniverse/grommet-theme'
+
+const appTheme = deepMerge(baseTheme, zooTheme)
 
 export default function App () {
   return (
-    <Grommet plain>
+    <Grommet theme={appTheme}>
       <Box>
         <Heading>Zooniverse Community Catalog</Heading>
         <Box as='main'>
