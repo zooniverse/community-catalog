8367dbdf1f6e9bbcd59f2ec41be321b94b6c7037
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-12-12 21:53:33 +0800

    Change Tester to a Grommet Form. Add optional chaining

--- src/App/Tester/Tester.js
+++ src/App/Tester/Tester.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react'
-import { Box, Button, Paragraph as P, TextInput } from 'grommet'
+import { Button, Form, Paragraph as P, TextInput } from 'grommet'
 import { talkAPI } from '@zooniverse/panoptes-js'
 
 export default function Tester () {
   const [ query, setQuery ] = useState('')
 
   function textQuery_onChange (e) {
-    setQuery(e.target.value)
+    setQuery(e?.target?.value || '')
   }
 
-  function buttonSubmit_onClick (e) {
-    console.log(query)
+  function form_onSubmit () {
+    console.log('+++ SUBMITTED: ', query)
   }
 
   return (
-    <Box>
+    <Form
+      onSubmit={form_onSubmit}
+    >
       <P>Tester Component</P>
-      <TextInput className='text-query' onChange={textQuery_onChange} value={query} placeholder='Type in a query' />
-      <Button className='button-submit' primary label='Submit' onClick={buttonSubmit_onClick} />
-    </Box>
+      <TextInput
+        className='text-query'
+        onChange={textQuery_onChange}
+        placeholder='Type in a query'
+        value={query}
+      />
+      <Button
+        className='button-submit'
+        primary label='Submit'
+        type='submit'
+      />
+    </Form>
   )
 }
