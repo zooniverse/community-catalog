a0aa3357910e977d0dc0e102751740bf3dfbe708
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-01-03 21:31:57 +0800

    Change build's target directory

--- webpack.dev.js
+++ webpack.dev.js
@@ -20,7 +20,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
   module: {
