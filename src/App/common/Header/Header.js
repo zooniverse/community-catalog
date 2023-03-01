8e2a509bd564b32303343ed44d851843b66194f2
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-03-01 23:44:21 +0000

    Header: improve Zooniverse Logo and header title

--- src/App/common/Header/Header.js
+++ src/App/common/Header/Header.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Box } from 'grommet'
+import { Box, Button, Text, TextInput } from 'grommet'
+import { ZooniverseLogo } from '@zooniverse/react-components'
+import styled from 'styled-components'
+
+const HeaderText = styled(Text)`
+  text-transform: uppercase;
+`
 
 export default function Header () {
   return (
     <Box
+      as='header'
       background='dark-1'
+      direction='row'
+      gap='small'
+      pad='small'
     >
-      Communities and Crowds
-
+      <Box
+        align='center'
+        flex='false'
+        width='xsmall'
+      >
+        <ZooniverseLogo size='4em' style={{ color: '#00979d' }} />
+        <HeaderText textAlign='center'>Communities &amp; Crowds</HeaderText>
+      </Box>
+      <Button
+        background='light-1'
+        label='Project Home Page'
+        href='https://www.zooniverse.org'
+      />
+      <Button
+       background='light-1'
+        label='Project Home Page'
+        href='https://www.zooniverse.org'
+      />
+      <TextInput
+        
+      />
     </Box>
   )
 }
