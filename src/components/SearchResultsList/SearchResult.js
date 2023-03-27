fd76b338d2ce6732f547709caa125e563d39fed6
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-03-27 23:49:27 +0100

    SearchResult: add link to Subject page

--- src/components/SearchResultsList/SearchResult.js
+++ src/components/SearchResultsList/SearchResult.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Text } from 'grommet'
+import { Link } from 'react-router-dom'
 
 import SubjectImage from '@src/components/SubjectImage'
 import fetchSubject from '@src/helpers/fetchSubject.js'
@@ -14,14 +15,25 @@ export default function SearchResult ({
   }, [ subjectId ])
 
   return (
-    <Box
+    <Link
+      to={`/subject/${subjectId}`}
     >
-      <SubjectImage
-        subject={subjectData}
-        small={true}
-        width={200}
-        height={200}
-      />
-    </Box>
+      <Box
+        background='white'
+        elevation='small'
+        width='200px'
+        margin={{ bottom: 'small' }}
+      >
+        <SubjectImage
+          subject={subjectData}
+          small={true}
+          width={200}
+          height={200}
+        />
+        <Box pad='small'>
+          <Text color='drawing-pink'>Lorem Ipsum, I don't know what should be here</Text>
+        </Box>
+      </Box>
+    </Link>
   )
 }
