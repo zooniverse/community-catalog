468efe57884db67c9b1a00168b226274f7d4ff7d
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-05-09 15:10:27 +0100

    Build: add proper production config

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.ico$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      favicon: 'src/favicon.ico',
+      process: 'process/browser',
+    }),
+    HtmlWebpackPluginConfig
+  ],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+    fallback: {
+      'url': require.resolve('url/'),  // Required by panoptes-client
+    },
+  },
+}
