369b32655376ea0519d3c83e61b75134d9ff67b0
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-06-05 14:27:14 +0100

    Remove ununused AppStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { AppContext } from './AppContext.js'
-import { initAppStore } from './AppStore.js'
 import { useStores } from './useStores.js'
 
-export { AppContext, initAppStore, useStores }
+export { AppContext, useStores }
