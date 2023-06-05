2a0d65178bc2f850b39a0a027cfdf3359c8772e2
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-06-05 22:33:00 +0100

    AppStore: remove 'initialised' (now a local state in App)

--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -2,16 +2,11 @@ import { types } from 'mobx-state-tree'
 
 const AppStore = types.model('AppStore', {
   
-  initialised: types.optional(types.boolean, false),
   project: types.maybe(types.frozen()),
   user: types.maybe(types.frozen()),
   
 }).actions(self => {
   return {
-    
-    setInitialised (val) {
-      self.initialised = val
-    },
 
     setProject (val) {
       self.project = val
