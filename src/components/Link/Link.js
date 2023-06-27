16ae8f4e07bb1e2d0a4261567a7adef6422b82b1
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-06-27 21:15:51 +0800

    FIX: Link now correctly preserves ?query param

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -18,7 +18,7 @@ export default function Link (props) {
   if (env) toUrl.push(`&env=${encodeURIComponent(env)}`)
 
   // Keep the query consistent, if the intended URL doesn't already have its own query
-  if (query && !to.match(/[\?&]env=/ig)) toUrl.push(`&env=${encodeURIComponent(query)}`)
+  if (query && !to.match(/[\?&]query=/ig)) toUrl.push(`&query=${encodeURIComponent(query)}`)
   
   return (
     <BaseLink
