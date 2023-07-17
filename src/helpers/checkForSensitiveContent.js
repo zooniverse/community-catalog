a74357a6e14d2235acaa8ef6b5e8a63e831a1a66
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-07-17 23:51:48 +0800

    checkForSensitiveContent: implement full functionality

--- src/helpers/checkForSensitiveContent.js
+++ src/helpers/checkForSensitiveContent.js
@@ -8,6 +8,16 @@ Inputs:
 Outputs:
 - (bool) true if ANY of the Subject's metadata fields meets the project config's
   sensitive_content_conditions clauses. False by default.
+
+For example, the following config will mark any subject that's red, blue,
+sweet, or sour as "sensitive":
+
+exampleProjectConfig = {
+  sensitive_content_conditions: [
+    { field: 'colour', values: ['red', 'blue' ] },
+    { field: 'flavour', values: ['sweet', 'sour' ] },
+  ]
+}
  */
 
 
@@ -16,6 +26,15 @@ export default function checkForSensitiveContent (
   project
 ) {
   if (!subject || !project) return false
+
+  const metadata = subject.metadata || {}
+  const conditions = project.sensitive_content_conditions || []
+
+  for (let i = 0 ; i < conditions.length ; i ++) {
+    const { field, values: valuesToCheckFor } = conditions[i]
+    const metadataValue = metadata[field]
+    if (valuesToCheckFor.includes(metadataValue)) return true
+  }
 
   return false
 }
