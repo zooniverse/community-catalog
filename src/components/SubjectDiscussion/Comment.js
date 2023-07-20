c2ecac00ff8a5c5b773d06ef74e82aa31583665f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-07-20 23:17:59 +0800

    Comment: add user avatar styling

--- src/components/SubjectDiscussion/Comment.js
+++ src/components/SubjectDiscussion/Comment.js
@@ -1,7 +1,8 @@
-import { Box, Text } from 'grommet'
+import { Box, Image, Text } from 'grommet'
 import { Markdownz } from '@zooniverse/react-components'
 
-import { ZOONIVERSE_URL } from '@src/config.js'
+import { ZOONIVERSE_URL, DEFAULT_AVATAR_URL } from '@src/config.js'
+import strings from '@src/strings.json'
 
 export default function Comment ({
   comment,
@@ -11,16 +12,46 @@ export default function Comment ({
 }) {
   if (!comment || !author) return null
 
+  const imgSrc = undefined
+  const displayName = author.display_name || author.login
+
   console.log('+++ author: ', author)
-  console.log('+++ roles: ', authorRoles)
+  console.log('+++ comment: ', comment)
 
   return (
     <Box
-      background='light-2'
+      background='light-1'
+      className='comment'
+      direction='row'
+      gap='xsmall'
+      margin={{ vertical: 'xsmall' }}
+      pad='xsmall'
     >
-      <Text>{author.display_name}</Text>
-      {authorRoles.map(role => <Text key={`comment-${comment.id}-${role.name}`}>{role.name}</Text>)}
-      <Markdownz baseURI={`${projectUrl}`}>{comment.body}</Markdownz>
+      <Box
+        border={{ color: 'orange' }}
+        flex={false}
+        direction='column'
+        width='120px'
+        justify='start'
+      >
+        <Image
+          alt={strings.components.subject_discussion.user_avatar.replace(/{username}/g, displayName)}
+          alignSelf='center'
+          src={imgSrc}
+          fallback={DEFAULT_AVATAR_URL}
+          width='80px'
+        />
+        <Box>
+          {(author.display_name) && <Text weight='bold'>{displayName}</Text>}
+          <Text>@{author.login}</Text>
+        </Box>
+        <Box>
+          {authorRoles.map(role => <Text key={`comment-${comment.id}-${role.name}`} size='xsmall'>{role.name}</Text>)}
+        </Box>
+      </Box>
+      <Box border={{ color: 'yellow' }}>
+        <Markdownz baseURI={`${projectUrl}`}>{comment.body}</Markdownz>
+      </Box>
     </Box>
   )
 }
--- src/config.js
+++ src/config.js
@@ -1,13 +1,23 @@
+// General stuff
 export const ZOONIVERSE_URL = 'https://www.zooniverse.org'
+
+// Database stuff
 export const DATABASE_URL = 'https://subject-set-search-api.zooniverse.org/'
 export const DATABASE_NAME = 'projects'
 export const TABLE_PREFIX = 'proj_'
 export const SUBJECT_ID_KEY = 'subject_id'
 export const KEYWORDS_KEY = '#keywords'
-export const SUBJECT_IMAGE_SIZE = 186
 export const PAGE_SIZE = 10  // When fetching resources from Panoptes or the database, request this many items per page.
+
+// Talk stuff
+export const DEFAULT_AVATAR_URL = 'https://static.zooniverse.org/www.zooniverse.org/assets/simple-avatar.png'
+
+// Layout stuff
+export const SUBJECT_IMAGE_SIZE = 186
 export const LAYOUT_MAIN_MAX_WIDTH = 1280  // App's <main> element will not exceed this width.
 export const NARROW_VIEW_WIDTH = 800  // At this width of below, we'll use a narrow (responsive mobile) view.
+
+// Data fetch stuff
 export const ASYNC_STATES = {
   READY: 'ready',
   FETCHING: 'fetching',
