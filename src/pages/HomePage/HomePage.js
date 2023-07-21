dafcde53bcd4d4a8f3a9fc1f470e2ba7b1828c44
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-07-22 04:06:12 +0800

    HomePage: add background image

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,13 +2,23 @@ import { Box, Text } from 'grommet'
 import styled from 'styled-components'
 
 import ProjectCard from '@src/components/ProjectCard'
-import Link from '@src/components/Link'
 import projectsJson from '@src/projects.json'
 
 import strings from '@src/strings.json'
+import { HOME_PAGE_BACKGROUND_IMAGE_URL } from '@src/config.js'
 
 const IntroBox = styled(Box)`
   max-width: 36.5em;
+`
+
+const ProjectsContainerOuter = styled(Box)`
+  background: #808080 url(${HOME_PAGE_BACKGROUND_IMAGE_URL});
+  background-size: 120% auto;
+  background-position: center 30%;
+`
+
+const ProjectsContainer = styled(Box)`
+  backdrop-filter: sepia(5%) hue-rotate(120deg) blur(6px);
 `
 
 function HomePage () {
@@ -39,20 +49,21 @@ function HomePage () {
           {strings.pages.home_page.pick_a_project}
         </Text>
       </Box>
-      <Box
-        direction='row'
-        wrap={true}
-        justify='center'
-        pad='small'
-        background='red'
-      >
-        {projectsJson.projects.map(proj => (
-          <ProjectCard
-            project={proj}
-            key={`project-${proj.id}`} 
-          />
-        ))}
-      </Box>
+      <ProjectsContainerOuter>
+        <ProjectsContainer
+          direction='row'
+          wrap={true}
+          justify='center'
+          pad='small'
+        >
+          {projectsJson.projects.map(proj => (
+            <ProjectCard
+              project={proj}
+              key={`project-${proj.id}`} 
+            />
+          ))}
+        </ProjectsContainer>
+      </ProjectsContainerOuter>
       <Box background='white' pad='small' />
     </Box>
   )
