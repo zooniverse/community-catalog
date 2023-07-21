0c57eef8b320fe0027335fe415e0be25f0dc40d3
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-07-22 03:27:32 +0800

    ProjectCard: present information, add hover/focus styles

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,25 +2,69 @@ import { Box, Text } from 'grommet'
 import styled from 'styled-components'
 
 import Link from '@src/components/Link'
+import { PROJECT_CARD_HEIGHT, PROJECT_CARD_WIDTH } from '@src/config.js'
 
 const ProjectLink = styled(Link)`
   text-decoration: none;
+
+  .project-card-description {
+    display: none;
+  }
+
+  &:hover, &:focus {
+    .project-card-description {
+      display: block;
+    }
+  }
+`
+
+const TitleText = styled(Text)`
+  text-transform: uppercase;
 `
 
 export default function ProjectCard ({
   project
 }) {
   if (!project) return null
+  
+  const background = (project.avatar)
+    ? `url(${project.avatar})`
+    : 'dark-2'
 
   return (
-    <ProjectLink keepQuery={false} to={`/projects/${project.slug}`}>
+    <ProjectLink
+      className='project-card'
+      keepQuery={false}
+      to={`/projects/${project.slug}`}
+    >
       <Box
-        width='200px'
-        background='light-1'
-        pad='small'
+        width={`${PROJECT_CARD_WIDTH}px`}
+        height={`${PROJECT_CARD_HEIGHT}px`}
+        overflow='hidden'
+        background={background}
         margin='small'
+        justify='end'
       >
-        <Text>{project.name}</Text>
+        <Box
+          background='white'
+          pad='small'
+        >
+          <TitleText
+            color='dark-5'
+            textAlign='center'
+            weight='bold'
+          >
+            {project.name}
+          </TitleText>
+          <Text
+            className='project-card-description'
+            color='dark-5'
+            margin={{ top: 'small' }}
+            textAlign='center'
+          >
+            {project.description}
+          </Text>
+        </Box>
       </Box>
     </ProjectLink>
   )
