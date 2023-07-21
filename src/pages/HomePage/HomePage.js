5145c35325327f14433680cf3154c64e2de17f71
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-07-22 02:23:49 +0800

    HomePage: setup new layout and add intro text

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,25 +1,64 @@
-import { Box } from 'grommet'
+import { Box, Text } from 'grommet'
+import styled from 'styled-components'
 
 import Link from '@src/components/Link'
 import projectsJson from '@src/projects.json'
+
+import strings from '@src/strings.json'
+
+const IntroBox = styled(Box)`
+  max-width: 36.5em;
+`
 
 function HomePage () {
   return (
     <Box
       background='dark-1'
       className='home-page'
-      gap='small'
-      pad='small'
     >
-      {projectsJson.projects.map(proj => (
-        <Box
-          key={`project-${proj.id}`}
-          background='light-1'
-          pad='small'
+      <IntroBox
+        direction='column'
+        alignSelf='center'
+        pad='medium'
+      >
+        <Text color='white'>{strings.pages.home_page.intro}</Text>
+      </IntroBox>
+      <Box
+        background='white'
+        direction='column'
+        align='center'
+        alignContent='center'
+        justify='center'
+        pad='small'
+      >
+        <Text
+          color='black'
+          size='large'
+          weight='bold'
         >
-          <Link keepQuery={false} to={`/projects/${proj.slug}`}>{proj.name}</Link>
-        </Box>
-      ))}
+          {strings.pages.home_page.pick_a_project}
+        </Text>
+      </Box>
+      <Box
+        direction='row'
+        wrap={true}
+        justify='center'
+        pad='small'
+        background='red'
+      >
+        {projectsJson.projects.map(proj => (
+          <Box
+            width='200px'
+            key={`project-${proj.id}`}
+            background='light-1'
+            pad='small'
+            margin='small'
+          >
+            <Link keepQuery={false} to={`/projects/${proj.slug}`}>{proj.name}</Link>
+          </Box>
+        ))}
+      </Box>
+      <Box background='white' pad='small' />
     </Box>
   )
 }
