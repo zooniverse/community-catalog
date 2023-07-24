145cdbe797e01f119fa618c48b7f2b0a01220234
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-07-25 01:58:00 +0800

    ErrorPage: provide more details of errors

--- src/App/ErrorPage.js
+++ src/App/ErrorPage.js
@@ -3,15 +3,25 @@ Error page. Keep this as simple as possible.
  */
 
 import { useRouteError } from 'react-router-dom'
+import strings from '@src/strings.json'
 
 export default function ErrorPage () {
   const error = useRouteError()
-  console.error('ErrorPage',error)
+  console.error('ErrorPage', error)
+
+  let title = strings.errors.general_error
+  let details = error?.toString()
+
+  if (error?.status) {
+    
+    title = `${error.status} ${error.statusText}` 
+    details = error.error?.message || details
+  }
 
   return (
     <>
-      <h1>Error</h1>
-      <p>{error?.toString()}</p>
+      <h2>{title}</h2>
+      <p>{details}</p>
     </>
   )
 }
