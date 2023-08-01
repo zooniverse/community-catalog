814029d11aaf0dd6f0cc139257117056e832ab67
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-08-02 03:10:27 +0800

    Footer: optimise logo sizes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,11 +8,19 @@ import logoScienceMediaMuseum from '@src/images/logo-science-and-media-museum.pn
 import logoAhrc from '@src/images/logo-ahrc.png'
 
 const TextBox = styled(Box)`
-  max-width: 36.5em;
+  max-width: 32.5em;
   
   p {
     font-size: 0.75em;
     text-align: center;
+  }
+`
+
+const LogoBox = styled(Box)`
+  img {
+    object-fit: contain;
+    max-width: 100%;
+    margin: 10px;
   }
 `
 
@@ -28,25 +36,31 @@ export default function Footer () {
       pad='small'
       wrap={true}
     >
-      <Box
+      <LogoBox
+        direction='row'
         flex={true}
-        width='small'
+        justify='end'
+        pad='small'
       >
-        <Image src={logoZooniverse} height='small' />
-      </Box>
+        <img src={logoZooniverse} height='40px' width='166px' />
+      </LogoBox>
       <TextBox
         flex={false}
         pad={{ horizontal: 'medium' }}
       >
         <Markdownz>{strings.components.footer}</Markdownz>
       </TextBox>
-      <Box
+      <LogoBox
+        direction='row'
         flex={true}
-        width='small'
+        justify='start'
+        overflow='hidden'
+        pad='small'
+        wrap={true}
       >
-        <Image src={logoAhrc} height='small' />
-        <Image src={logoScienceMediaMuseum} height='small' />
-      </Box>
+        <img src={logoAhrc} height='40px' width='157px' />
+        <img src={logoScienceMediaMuseum} height='40px' width='40px' />
+      </LogoBox>
     </GrommetFooter>
   )
 }
