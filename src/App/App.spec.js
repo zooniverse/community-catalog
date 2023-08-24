02ba85b720ecbbd00527715276593d2260cb8b85
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-08-24 22:59:57 +0800

    App.spec: remove unused tests

--- src/App/App.spec.js
+++ src/App/App.spec.js
@@ -17,27 +17,4 @@ describe('App', function () {
     const logo = await screen.findByText(/Zooniverse Logo/i)
     expect(logo).toBeInTheDocument()
   })
-
-  /*
-  describe('when user isn\'t logged in', function () {
-    test('should display "isn\'t logged in" message', async function () {
-      auth.checkCurrent.mockResolvedValue(null)
-      render(<App />)
-      const userText = await screen.findByText(/User isn't logged in/i)
-      expect(userText).toBeInTheDocument()
-    })
-  })
-
-  describe('when user is logged in', function () {
-    test('should display user details', async function () {
-      auth.checkCurrent.mockResolvedValue({
-        display_name: 'Test User',
-        login: 'testuser',
-      })
-      render(<App />)
-      const userText = await screen.findByText(/Logged in as Test User/i)
-      expect(userText).toBeInTheDocument()
-    })
-  })
-  */
 })
