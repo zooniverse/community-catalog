1ed750c80bd4a2269f2fa5b0a33f62e3e4939d82
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-08-25 18:34:48 +0800

    ProjectContainer tests: now checks if project is set

--- src/components/ProjectContainer/ProjectContainer.spec.js
+++ src/components/ProjectContainer/ProjectContainer.spec.js
@@ -2,22 +2,26 @@ import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route, Routes } from 'react-router-dom' 
 import ProjectContainer from './ProjectContainer.js'
 
-import projectsJson from '@src/projects.json'
-const testProject = projectsJson.projects.find(p => p.slug === 'darkeshard/community-catalog')
-
 import { AppContext } from '@src/store'
 
 describe.only('ProjectContainers', function () {
+  let selectedProject
   const store = {
-    project: testProject,
+    project: {
+      name: 'Placeholder Project',
+      slug: 'foo/bar',
+      id: '1234'
+    },
     user: null,
     showingSensitiveContent: false,
-    setProject: (p) => { console.log('+++ p: ', p) },
+    setProject: (p) => { selectedProject = p },
     setUser: () => {},
     setShowingSensitiveContent: () => {},
   }
 
   test('should find a project based on the route/path', async function () {
+    selectedProject = null
+
     render(
       <AppContext.Provider value={store}>
         <MemoryRouter initialEntries={[
@@ -33,6 +37,6 @@ describe.only('ProjectContainers', function () {
       </AppContext.Provider>
     )
 
-    // TODO: check if the project is set.
+    expect(selectedProject?.name).toEqual('Community Catalog (Stable Test Project)')
   })
 })
