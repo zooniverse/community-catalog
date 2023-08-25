1aeb9d5fc584c52ae0691b0fe70a2416fe164e4f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-08-25 21:31:10 +0800

    Jest: cleanup, refactor

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,26 @@
 const config = {
-  "moduleNameMapper": {
+  'moduleNameMapper': {
     // Maps all media files to a mock file.
     // Without this, test will crash with a "Jest encountered an unexpected token" due to Jest having no idea what to do with binary files.
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest/mockFile.js",
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest/mockMediaFile.js',
 
     // Maps the @src alias added in webpack.
-    "^@src/(.*)$": "<rootDir>/src/$1"
+    '^@src/(.*)$': '<rootDir>/src/$1'
   },
 
   // Tells Jest to look for unit test files in the /src directory.
-  "roots": [
-    "<rootDir>/src"
+  'roots': [
+    '<rootDir>/src'
   ],
 
-  "setupFilesAfterEnv": [
-    "<rootDir>/jest/jest-setup.js"
+  'setupFilesAfterEnv': [
+    '<rootDir>/jest/jest-setup.js'
   ],
 
   // Without this, render() will result in "document is not defined" errors.
-  "testEnvironment": "jsdom"
+  'testEnvironment': 'jsdom',
 }
 
-// Jest doesn't like "export" (i.e. ES6 modules), so we use module.exports (CommonJS modules).
+// Jest doesn't like 'export' (i.e. ES6 modules), so we use module.exports (CommonJS modules).
 // Probably because it's running in Node.js.
 module.exports = config
--- src/components/SubjectImage/SubjectImage.spec.js
+++ src/components/SubjectImage/SubjectImage.spec.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+// import SubjectImage from './SubjectImage.js'
 
 describe('Subject Image', function () {
   test('should render subject image', async function () {
