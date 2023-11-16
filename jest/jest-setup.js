2f8a16115cd0d40a5f3fd7f239929a06b90af0d6
Jim O'Donnell <jim@zooniverse.org>
2023-11-16 14:56:20 +0000

    Explain the Jest config

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -1,6 +1,10 @@
 import '@testing-library/jest-dom'  // Import various matchers for DOM test, e.g. `expect(headerElement).toBeInTheDocument()`
 
-// whatwg-url expects TextDecoder and TextEncoder to be available globally.
+/*
+whatwg-url expects Node's TextDecoder and TextEncoder to be available globally.
+They aren't defined in the `jsdom` environment, so patch them here.
+https://github.com/jsdom/whatwg-url/issues/197
+*/
 import { TextDecoder, TextEncoder } from 'util';
 
 global.TextDecoder = TextDecoder;
