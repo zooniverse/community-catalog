f3e764f0ba05306a2a86f495b97c2b6d3eac2991
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-08-02 15:57:51 +0100

    WorkflowSelectionDialog: now lists workflows to choose from

--- src/components/WorkflowSelectionDialog/WorkflowSelectionDialog.js
+++ src/components/WorkflowSelectionDialog/WorkflowSelectionDialog.js
@@ -1,6 +1,14 @@
-import { Box, Layer, Paragraph as P } from 'grommet'
+import { Anchor, Box, Layer, Paragraph as P, Text } from 'grommet'
+import styled from 'styled-components' 
+import { Article as ClassifyIcon } from 'grommet-icons'
 
 import strings from '@src/strings.json'
+
+const StyledLink = styled(Anchor)`
+  color: #000000;
+  text-decoration: none;
+  text-transform: uppercase;
+`
 
 export default function WorkflowSelectionDialog ({
   project = undefined,
@@ -8,7 +16,12 @@ export default function WorkflowSelectionDialog ({
   show = false,
   onClose = () => {}
 }) {
-  if (!show) return null
+  if (!show || !project || !subject) return null
+
+  if (!Array.isArray(project.classify_url)) {
+    console.error('<WorkflowSelectionDialog>', strings.errors.expected_classify_url_to_be_array)
+    throw new Error(strings.errors.expected_classify_url_to_be_array)
+  }
 
   return (
     <Layer
@@ -20,9 +33,24 @@ export default function WorkflowSelectionDialog ({
       <Box
         background='light-1'
         border={true}
-        pad="large"
+        pad={{ top: "small", right: "large", bottom: "large", left: "large" }}
       >
         <P>{strings.components.workflow_selection_dialog.choose_your_workflow}</P>
+        {project.classify_url.map((({ label, value }) => {
+          const classifySubjectUrl = value?.replace(/{subject_id}/g, subject.id)
+          return (
+            <Box>
+              <StyledLink
+                gap='xsmall'
+                href={classifySubjectUrl}
+                icon={<ClassifyIcon size='small' />}
+                label={<Text>{label}</Text>}
+                margin='small'
+              />
+            </Box>
+          )
+        }))}
+
       </Box>
     </Layer>
   )
